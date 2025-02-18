d170659859bc2955c6bd3c373b040b53b5b40769
Olcay Davut Cabbas <olcay.d.cabbas@gmail.com>
2025-02-18 15:22:58 +0300

    Update lock_free_stack to hazard pointer

--- lock_free_stack.hpp
+++ lock_free_stack.hpp
@@ -1,42 +1,77 @@
 #include <atomic>
 #include <optional>
+#include <vector>
+#include <thread>
 
 template <typename T>
 struct lock_free_stack {
 private:
     struct node {
-        explicit node(const T& v) : val(v), next(nullptr) {} // Properly initialize val
+        explicit node(const T& v) : val(v), next(nullptr) {}
         T val;
         node* next;
     };
 
-    std::atomic<node*> head;
+    struct tagged_pointer {
+        node* ptr;
+        std::uintptr_t tag;
+    };
+
+    std::atomic<tagged_pointer> head;
+
+    // Hazard pointer array (simplified, ideally use a thread-local hazard pointer system)
+    static constexpr int MAX_THREADS = 8;  // Adjust based on expected concurrency
+    std::atomic<node*> hazard_pointers[MAX_THREADS];
 
 public:
-    lock_free_stack() : head(nullptr) {} // Initialize head
+    lock_free_stack() {
+        head.store({nullptr, 0});
+        for (auto& hp : hazard_pointers) {
+            hp.store(nullptr);
+        }
+    }
 
     void push(const T& val) {
         node* new_node = new node(val);
+        tagged_pointer old_head;
         do {
-            new_node->next = head.load(std::memory_order_relaxed);
-        } while (!head.compare_exchange_weak(new_node->next, new_node, 
-                                             std::memory_order_release, 
+            old_head = head.load(std::memory_order_relaxed);
+            new_node->next = old_head.ptr;
+        } while (!head.compare_exchange_weak(old_head, {new_node, old_head.tag + 1},
+                                             std::memory_order_release,
                                              std::memory_order_relaxed));
     }
 
     std::optional<T> pop() {
-        node* old_head;
+        int thread_id = std::hash<std::thread::id>{}(std::this_thread::get_id()) % MAX_THREADS;
+        node* old_node;
+
+        tagged_pointer old_head;
         do {
-            old_head = head.load(std::memory_order_acquire); // at his time we use acquire to get updated value from push
-            if (old_head == nullptr) {
+            old_head = head.load(std::memory_order_acquire);
+            if (old_head.ptr == nullptr) {
                 return std::nullopt;
             }
-        } while (!head.compare_exchange_weak(old_head, old_head->next, 
-                                             std::memory_order_release, 
+            hazard_pointers[thread_id].store(old_head.ptr, std::memory_order_seq_cst);
+        } while (!head.compare_exchange_weak(old_head, {old_head.ptr->next, old_head.tag + 1},
+                                             std::memory_order_release,
                                              std::memory_order_relaxed));
 
-        T return_value = old_head->val;
-        delete old_head; // Correct deallocation
-        return return_value;
+        hazard_pointers[thread_id].store(nullptr, std::memory_order_seq_cst);
+
+        // **DELAYED DELETION**
+        bool can_delete = true;
+        for (int i = 0; i < MAX_THREADS; ++i) {
+            if (hazard_pointers[i].load(std::memory_order_seq_cst) == old_head.ptr) {
+                can_delete = false;
+                break;
+            }
+        }
+
+        if (can_delete) {
+            delete old_head.ptr;  // Safe to delete now
+        }
+
+        return old_head.ptr->val;
     }
 };
