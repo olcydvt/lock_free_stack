a7ce27df237782adeb81c7f8479a81027db76185
Olcay Davut Cabbas <olcay.d.cabbas@gmail.com>
2025-02-18 14:41:50 +0300

    Update lock_free_stack.hpp

--- lock_free_stack.hpp
+++ lock_free_stack.hpp
@@ -1,24 +1,42 @@
+#include <atomic>
+#include <optional>
+
 template <typename T>
-struct Stack {
-    struct Node {
-        T t;
-        std::shared_ptr<Node> next;
+struct lock_free_stack {
+private:
+    struct node {
+        explicit node(const T& v) : val(v), next(nullptr) {} // Properly initialize val
+        T val;
+        node* next;
     };
 
-    std::atomic<std::shared_ptr<Node>> head;
+    std::atomic<node*> head;
 
-    void push_front(T t) {
-        auto p = std::make_shared<Node>(std::move(t), head.load());
-        while (!head.compare_exchange_weak(p->next, p)) {}
+public:
+    lock_free_stack() : head(nullptr) {} // Initialize head
+
+    void push(const T& val) {
+        node* new_node = new node(val);
+        do {
+            new_node->next = head.load(std::memory_order_relaxed);
+        } while (!head.compare_exchange_weak(new_node->next, new_node, 
+                                             std::memory_order_release, 
+                                             std::memory_order_relaxed));
     }
 
-    std::optional<T> pop_front() {
-        auto p = head.load();
-        while (p != nullptr && !head.compare_exchange_weak(p, p->next)) {}
-        if (p != nullptr)
-            return { std::move(p->t) };
-        else
-            return {};
-    }
+    std::optional<T> pop() {
+        node* old_head;
+        do {
+            old_head = head.load(std::memory_order_acquire); // at his time we use acquire to get updated value from push
+            if (old_head == nullptr) {
+                return std::nullopt;
+            }
+        } while (!head.compare_exchange_weak(old_head, old_head->next, 
+                                             std::memory_order_release, 
+                                             std::memory_order_relaxed));
 
+        T return_value = old_head->val;
+        delete old_head; // Correct deallocation
+        return return_value;
+    }
 };
